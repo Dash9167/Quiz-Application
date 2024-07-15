aaadb3ceef6a19be2a7c0e4f06401e4c811d4690
Dash9167 <105582673+Dash9167@users.noreply.github.com>
2024-07-16 01:51:43 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,7 @@
+// JavaScript to handle menu toggle
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function() {
+  nav.classList.toggle('active');
+});
